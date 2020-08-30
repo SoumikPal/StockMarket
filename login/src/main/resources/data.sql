93c5406a7041b97430d40cbbf1e52dece7fe1162
Soumik Pal <pal.soumik33@gmail.com>
2020-08-31 00:38:35 +0530

    Third Commit

---
+++ login/src/main/resources/data.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS Contact;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Contact (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  city varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  phone varchar(255) DEFAULT NULL,
+  state varchar(255) DEFAULT NULL,
+  zip varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS User;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE User (
+   id int(11) NOT NULL AUTO_INCREMENT,
+   confirmed int(11) DEFAULT NULL,
+   password varchar(20) DEFAULT NULL,
+   userType varchar(255) DEFAULT NULL,
+   userName varchar(20) NOT NULL,
+   contactId int(11) DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (contactId) REFERENCES Contact(id)
+);
+
+
+INSERT INTO Contact VALUES
+	  (1,'Kolkata','xyz@gmail.com','9999999999','WB','700107'),
+	  (2,'Kolkata','xxx@gmail.com','8888888888','WB','700107');
+	  
+INSERT INTO User VALUES
+	  (1,1,'pppppppp','ADMIN','MAX','1'),
+	  (2,0,'zzzzzzzz','USER','MAXY','2');
+	  
+	  
