36007fa17dfe7dabe2900e5ac5d28fb3b1ba680f
waasef <36700115+waasef@users.noreply.github.com>
2020-09-09 00:37:42 +0530

    Extra Functionalities Added

--- stockmarket(Angular)/src/app/stock/stock.component.ts
+++ stockmarket(Angular)/src/app/stock/stock.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { stock } from './stock.model';
 
 @Component({
   selector: 'app-stock',
@@ -13,6 +14,18 @@ export class StockComponent implements OnInit {
   mainbutton: Boolean;
   searchid: String;
   button1: Boolean; button2:Boolean; button3: Boolean;
+  s_id: number; s_cp: number; s_op: number; s_to: number; s_cid: number; s_ex: String;
+  json;
+  newstock: stock={
+    id : undefined,
+    close_price: undefined,
+  	company_turnover : undefined,
+    date: undefined,
+    date_time: undefined,
+    open_price : undefined,
+    company_id: undefined,
+    stock_exchange: undefined
+  };
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -43,5 +56,19 @@ export class StockComponent implements OnInit {
   }
   getbycompanyid(){
     this.http.get('http://localhost:8080/stock/stockbycompanyid/'+this.searchid).subscribe(response =>this.res2=response);
+  }
+  additem(){
+    this.newstock.id=this.s_id;
+    this.newstock.close_price=this.s_cp;
+    this.newstock.open_price=this.s_op;
+    this.newstock.company_turnover=this.s_to;
+    this.newstock.stock_exchange=this.s_ex.split(",").map(x=>+x);
+    this.newstock.company_id=this.s_cid;
+    this.newstock.date=new Date();
+    this.newstock.date_time=new Date();
+    this.http.post('http://localhost:8080/stock/',this.newstock).toPromise().then((d:any)=>{
+      console.log(d);
+      this.json=JSON.stringify(d.json);
+    });
   }
 }
